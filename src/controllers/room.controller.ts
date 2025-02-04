d227084ce2531f78f7d47ee2ceea5d2a063025c5
aryarobyy <robyaryanata12345@gmail.com>
2025-02-04 23:03:01 +0700

    post, put, delete room controller

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -44,3 +44,88 @@ export const getDetailRoom = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const postRoom = async (req: Request, res: Response) => {
+    const { title, description, image, authorId, } = req.body;
+    try {
+        const room = await prisma.room.create({
+            data: {
+                title,
+                description,
+                image,
+                authorId,
+            },
+            select: {
+                title: true,
+                description: true,
+                image: true,
+                authorId: true,
+                id: true
+            }
+        })
+
+        res.status(200).json(
+            successResponse(room)
+        );
+
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        console.error(errMessage)
+        res.status(500).json(
+            errorResponse(500, 'Internal Server Error', error, errMessage)
+        )
+    }
+}
+
+export const updateRoom = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title, description, image } = req.body;
+
+    try {
+        const room = await prisma.room.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                description,
+                image,
+            },
+            select: {
+                title: true,
+                description: true,
+                image: true
+            }
+        })
+        res.status(200).json(
+            successResponse(room)
+        )
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        console.error(errMessage)
+        res.status(500).json(
+            errorResponse(500, 'Internal Server Error', error, errMessage)
+        )
+    }
+}
+
+export const deleteRoom = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const room = await prisma.room.delete({
+            where: {
+                id
+            }
+        })
+        res.status(200).json(
+            successResponse(null, `${room.description} deleted`)
+        );
+    } catch (error) {
+        const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        console.error(errMessage)
+        res.status(500).json(
+            errorResponse(500, 'Internal Server Error', error, errMessage)
+        )
+    }
+}

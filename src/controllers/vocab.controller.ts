1ec53278105bf35221b6c1c2a99ce2eb79fc40ae
zaxchaxs <zaxchaxsecondid@gmail.com>
2025-02-17 08:42:38 +0700

    Add alternative getting sign code using query

--- src/controllers/vocab.controller.ts
+++ src/controllers/vocab.controller.ts
@@ -1,11 +1,18 @@
+import { Prisma } from "@prisma/client";
 import prisma from "../configs/prismaClient";
 import { errorResponse, successResponse } from "../utils/response";
 import { Request, Response } from "express";
 
 export const getAllVocab = async (req: Request, res: Response) => {
     try {
-        const vocabs = await prisma.vocab.findMany();
-        res.status(200).json(successResponse(vocabs))
+        const signCode = Array.isArray(req.query.signCode) ? req.query.signCode[0] : req.query.signCode;
+
+        const whereClouse: Prisma.VocabWhereInput = signCode ? { signCode } : {};
+        
+        const vocabs = await prisma.vocab.findMany({
+            where: whereClouse
+        });
+        res.status(200).json(successResponse(vocabs));
     } catch (error) {
         const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
         console.error(errMessage)
@@ -38,7 +45,7 @@ export const getVocab = async (req: Request, res: Response) => {
 };
 
 export const getVocabsBySignCode = async (req: Request, res: Response) => {
-    try{
+    try {
         const { signCode } = req.params;
         const vocabs = await prisma.vocab.findMany({
             where: {

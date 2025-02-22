6f419cb938ab3f69bac2a3406c57a5ec2d5a0a7a
aryarobyy <robyaryanata12345@gmail.com>
2025-02-22 15:53:26 +0700

    Add get by email and username

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteUser, getDetailUser, getUsers, loginUser, postUser, updateUser, verifyTokenUser } from "../controllers/user.controller";
+import { deleteUser, getDetailUser, getUserByEmail, getUserByUsername, getUsers, loginUser, postUser, updateUser, verifyTokenUser } from "../controllers/user.controller";
 import { deleteUserMiddleware, loginUserMiddleware, registUserMiddleware, updateUserMiddleware, verifyTokenMiddleware } from "../middlewares/users";
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getDetailUser)
+router.get('/username/:username', getUserByUsername)
+router.get('/email/:email', getUserByEmail)
 router.post('/', registUserMiddleware, postUser);
 router.put('/:id', updateUserMiddleware, updateUser);
 router.delete('/:id', deleteUserMiddleware, deleteUser);
